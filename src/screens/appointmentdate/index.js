13a23d4ffc8c860fcf782c6edc8ed849e9f1c69d
DevOski <designsfunnel@gmail.com>
2022-12-14 04:42:59 +0500

    updated

--- src/screens/appointmentdate/index.js
+++ src/screens/appointmentdate/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./appoint.css";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
@@ -12,16 +12,21 @@ import { format } from "date-fns";
 import { enGB } from "date-fns/locale";
 import { DatePickerCalendar } from "react-nice-dates";
 import "react-nice-dates/build/style.css";
+import moment from "moment";
 
 const Appointmentdate = () => {
   const [date, setDate] = useState();
   // const [date, setDate] = useState();
-  const [calendshow, setcalendshow] = useState(true)
+  const [calendshow, setcalendshow] = useState(true);
   const [slot, setSlot] = useState([]);
   const [trainer, setTrainer] = useState();
-  const Show =()=>{
-    setcalendshow(!calendshow)
-  }
+  const [currentDate, setCurrentDate] = useState("");
+  const [currentDTime, setCurrentTime] = useState("");
+  const [dateSlot, setDateSlot] = useState([]);
+
+  const Show = () => {
+    setcalendshow(!calendshow);
+  };
   const onSelectDatee = () => {
     if (date) {
       navigate("/question2");
@@ -45,7 +50,36 @@ const Appointmentdate = () => {
       navigate("/selectdatepage");
     }
   };
-  console.log(date,'=====>');
+  console.log(date, "=====>");
+  useEffect(() => {
+    var utc = new Date().toJSON().slice(0, 10).replace(/-/g, "-");
+    setCurrentDate(utc);
+    const time = new Date().getTime();
+    let currentTime = `${moment(time).format("hh:mma")}`;
+    // console.log(currentTime,utc);
+    setCurrentTime(currentTime);
+  }, []);
+
+  const getAllDateSlots = (date) => {
+    let selectedDate = moment(date).format("DD/MM/YYYY");
+    var myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+    var raw = JSON.stringify({
+      date: selectedDate,
+      time: currentDTime,
+    });
+    console.log(raw);
+    var requestOptions = {
+      method: "POST",
+      headers: myHeaders,
+      body: raw,
+      redirect: "follow",
+    };
+    fetch("http://alsyedmmtravel.com/api/all_trCalenderSlots", requestOptions)
+      .then((response) => response.json())
+      .then((result) => setDateSlot(result))
+      .catch((error) => console.log("error", error));
+  };
   return (
     <>
       <Container fluid>
@@ -81,37 +115,47 @@ const Appointmentdate = () => {
           </div> */}
       </Row>
       <Row class="d-flex justify-content-center flex-column align-items-center gap-5 pt-5">
-        <Col lg="12"className="d-flex justify-content-center align-items-center">
-         <div className="calbox" onClick={Show} ><p className="caltext">Calender</p></div>
-         <div className="slotbox" onClick={Show}  ><p className="caltext">List</p></div>
+        <Col
+          lg="12"
+          className="d-flex justify-content-center align-items-center"
+        >
+          <div className="calbox" onClick={Show}>
+            <p className="caltext">Calender</p>
+          </div>
+          <div className="slotbox" onClick={Show}>
+            <p className="caltext">List</p>
+          </div>
         </Col>
-      
       </Row>
       <Row>
-      <Col lg="12" className="d-flex justify-content-center">
-{calendshow ?<div className="calender">
-            <p>
-              Selected date:{" "}
-              {date ? format(date, "dd MMM yyyy", { locale: enGB }) : "none"}.
-            </p>
-            <DatePickerCalendar
-              date={date}
-              onDateChange={setDate}
-              locale={enGB}
-            />
-          </div>:<div className="slotdiv">
-            <p className="timeslot">Time:{slot}</p>
-          </div>}
-
-          
-          
-          
+        <Col lg="12" className="d-flex justify-content-center">
+          {calendshow ? (
+            <div className="calender">
+              <p>
+                Selected date:{" "}
+                {date ? format(date, "dd MMM yyyy", { locale: enGB }) : "none"}.
+              </p>
+              <DatePickerCalendar
+                date={date}
+                onDateChange={getAllDateSlots}
+                locale={enGB}
+              />
+              
+            </div>
+          ) : (
+            <div className="slotdiv">
+              <p className="timeslot">Time:{slot}</p>
+            </div>
+          )}
         </Col>
         <div className="classextbutton">
-            <button className="newpassbutt" onClick={onSelectDatee}>Next</button>
-          </div>
+          <button className="newpassbutt" onClick={onSelectDatee}>
+            Next
+          </button>
+        </div>
       </Row>
     </>
+  
   );
 };
 
