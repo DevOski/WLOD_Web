f0285c77a85335d55aee0e4c5b7c7766b2a7fae0
DevOski <designsfunnel@gmail.com>
2023-01-04 20:53:07 +0500

    updated

--- src/screens/videocalingscreen/index.js
+++ src/screens/videocalingscreen/index.js
@@ -1,22 +1,27 @@
 import React, { useEffect, useState } from "react";
 import AgoraUIKit from "agora-react-uikit";
-import { getTokenFromAPI, getUser } from "../../services/utilities/api";
+import {
+  createChannel,
+  getTokenFromAPI,
+  getUser,
+} from "../../services/utilities/api";
 import { useSelector } from "react-redux";
-import { useLocation } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 
 const Videocalling = () => {
-  
   const params = useLocation();
-
-console.log("response",params.state)
+  const userID = useSelector((state) => state.user.user_id);
+  const tr_id = params.state.tr_id;
+  console.log("----------->>>", userID, tr_id);
   const [videoCall, setVideoCall] = useState(false);
   const [channelName, setChannelName] = useState("");
   const [tokenAPI, setTokenAPI] = useState("");
-
+  let navigate = useNavigate();
   const usertoken = useSelector((state) => state.token);
   useEffect(() => {
-    console.log("123------------------->>>");
+    // console.log("123------------------->>>");
     getUserDetails();
+    channelCreate();
   }, []);
 
   const getUserDetails = async () => {
@@ -31,21 +36,67 @@ console.log("response",params.state)
       console.log(error);
     }
   };
+
+  const channelCreate = async () => {
+    try {
+      let response = await createChannel(tr_id, userID);
+      console.log("----------->>>>>>", response.data.message);
+      // if (response.data.message == "Channel created successfully") {
+
+      //   var myHeaders = new Headers();
+      //   myHeaders.append("Authorization", usertoken);
+      //   var formdata = new FormData();
+      //   formdata.append("tr_id", tr_id);
+      //   var requestOptions = {
+      //     method: "POST",
+      //     headers: myHeaders,
+      //     body: formdata,
+      //     redirect: "follow",
+      //   };
+
+      //   fetch(
+      //     "https://dashboard.weightlossondemand.com/backend/api/notification",
+      //     requestOptions
+      //   )
+      //     .then((response) => response.json())
+      //     .then((result) => {
+      //       console.log(result);
+      //       setTimeout(() => {
+      //         navigation.navigate('videocallingscreen', {
+      //           tr_id: route?.params?.tr_id,
+      //           tr_name: route?.params?.tr_name,
+      //           tr_image: route?.params?.tr_image,
+      //           tr_amount: route?.params?.tr_amount,
+      //         });
+      //       }, 5000);
+      //     })
+      //     .catch((error) => console.log("error", error));
+      // }
+    } catch (error) {
+      console.log(error);
+    }
+  };
   const rtcProps = {
     appId: "270b512970864b0a93b14650e52e8f9c",
     channel: channelName, // your agora channel
     token: tokenAPI, // use null or skip if using app in testing mode
   };
-  console.log(rtcProps,'====>rctprops');
+  console.log(rtcProps, "====>rctprops");
   const callbacks = {
-    EndCall: () => setVideoCall(false),
+    EndCall: () => {setVideoCall(false);navigate('/rating');}
+  };
+  const getChannel = () => {
+    // getUserDetails();
+
+    setVideoCall(true);
+    // alert("fo")
   };
   return videoCall ? (
     <div style={{ display: "flex", width: "100vw", height: "100vh" }}>
       <AgoraUIKit rtcProps={rtcProps} callbacks={callbacks} />
     </div>
   ) : (
-    <h3 onClick={() => setVideoCall(true)}>Start Call</h3>
+    <h3 onClick={getChannel}>Start Call</h3>
   );
 };
 
