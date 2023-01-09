7c6d5992076b1a81b829cafc4854a1dd5f0a7758
DevOski <designsfunnel@gmail.com>
2023-01-09 22:28:50 +0500

    updated

--- src/screens/confirmandpay/index.js
+++ src/screens/confirmandpay/index.js
@@ -65,7 +65,7 @@ const ConfirmAndPay = () => {
       }
       if (params?.state?.ApplyCupon != null) {
         setcuopontext("Cuopon Applied");
-        setcost("$2.5");
+        setcost("$25");
         setbstatus(true);
       }
     };
