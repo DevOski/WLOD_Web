1ff361d73b6126416fe2f41df5564664410fbad3
DevOski <designsfunnel@gmail.com>
2023-01-19 18:38:53 +0500

    vvideocall

--- src/screens/trainervideocall/index.js
+++ src/screens/trainervideocall/index.js
@@ -11,104 +11,21 @@ import { Button } from "react-bootstrap";
 import { Loader } from "../../component";
 
 const TrainerVideo = () => {
-  const [loder, setloder] = useState(false)
-  const params = useLocation();
-  const userID = useSelector((state) => state.user.user_id);
-  const tr_id = params.state.tr_id;
-  console.log("----------->>>", userID, tr_id);
   const [videoCall, setVideoCall] = useState(false);
-  const [channelName, setChannelName] = useState("");
-  const [tokenAPI, setTokenAPI] = useState("");
-  let navigate = useNavigate();
-  const usertoken = useSelector((state) => state.token);
-  useEffect(() => {
-    // console.log("123------------------->>>");
-    getUserDetails();
-    channelCreate();
-  }, []);
-
-  const getUserDetails = async () => {
-    try {
-      let response = await getUser(usertoken);
-      console.log("chane--->", response.data.data.channel);
-      setChannelName(response.data.data.channel);
-      let resToken = await getTokenFromAPI(response.data.data.channel);
-      let token = resToken.data.rtcToken;
-      setTokenAPI(token);
-    } catch (error) {
-      console.log(error);
-    }
-  };
-
-  const channelCreate = async () => {
-    try {
-      let response = await createChannel(tr_id, userID);
-      console.log("----------->>>>>>", response.data.message);
-      if (response.data.message == "Channel created successfully") {
-
-        var myHeaders = new Headers();
-        myHeaders.append("Authorization", usertoken);
-        var formdata = new FormData();
-        formdata.append("tr_id", tr_id);
-        var requestOptions = {
-          method: "POST",
-          headers: myHeaders,
-          body: formdata,
-          redirect: "follow",
-        };
-
-        fetch(
-          "https://dashboard.weightlossondemand.com/backend/api/notification",
-          requestOptions
-        )
-          .then((response) => response.json())
-          .then((result) => {
-            console.log(result);
-            setloder(false)
-          })
-          .catch((error) => console.log("error", error));
-      }
-    } catch (error) {
-      console.log(error);
-    }
-  };
   const rtcProps = {
-    appId: "270b512970864b0a93b14650e52e8f9c",
-    channel: channelName, // your agora channel
-    token: tokenAPI, // use null or skip if using app in testing mode
+    appId: '440edd9ffde44494a537d58b9d73bb1a',
+    channel: 'Testing', // your agora channel
+    token: '007eJxTYEhan8y16F/nrknVjnIbv39OOu51suO43rpdr08Yb/2xPfGmAoOJiUFqSoplWlpKqomJiaVJoqmxeYqpRZJlirlxUpJh4j7zLckNgYwMSi8OMTBCIYjPzhCSWlySmZfOwAAA4tYk3w==' // use null or skip if using app in testing mode
   };
-  
-  console.log(rtcProps, "====>rctprops");
   const callbacks = {
-    EndCall: () => {navigate('/rating',{
-      state : {
-        tr_id:tr_id,
-       
-      }
-    }
-    
-    );}
+    EndCall: () => setVideoCall(false),
   };
-  const getChannel = () => {
-    // getUserDetails();
-    setloder(true)
-
-    setVideoCall(true);
-    // setloder(false)
-  };
-  return videoCall  ? (
-    <div style={{ display: "flex", width: "100vw", height: "100vh" }}>
-      <AgoraUIKit rtcProps={rtcProps} callbacks={callbacks}    />
+  return videoCall ? (
+    <div style={{display: 'flex', width: '100vw', height: '100vh'}}>
+      <AgoraUIKit rtcProps={rtcProps} callbacks={callbacks} />
     </div>
   ) : (
-    <div className="d-flex justify-content-center align-items-center" style={{ display: "flex", width: "100vw", height: "100vh",}}>
-<div style={{width:"10%",}}>
-
-    <Button onClick={getChannel}>Join Session</Button>
-</div>
-{loder && <Loader />}
-    </div>
-  );
+    <h3 onClick={() => setVideoCall(true)}>Start Call</h3>)
 };
 
 export default TrainerVideo;
